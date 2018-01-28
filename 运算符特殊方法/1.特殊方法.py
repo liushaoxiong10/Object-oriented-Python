3690159eab0e812b8950a975dae88ef7cfc59032
liushaoxiong10 <liushaoxiong10@outlook.com>
2018-01-28 21:44:52 +0800

    No.7:运算符特殊方法

---
+++ 运算符特殊方法/1.特殊方法.py
@@ -0,0 +1,69 @@
+'''
+object.__add__(self, other)                        +
+object.__sub__(self, other)                        -
+object.__mul__(self, other)                        *
+object.__truediv__(self, other)                    /
+object.__floordiv__(self, other)                   //
+object.__mod__(self, other)                        %
+object.__divmod__(self,other)                      divmod()函数
+object.__pow__(self,other[,modulo])                pow()函数  **
+object.__neg__(self)                               -
+object.__pos__(self)                               +
+object.__abs__(self)                               abs()函数
+object.__complex(self)                             complex()函数
+object.__int__(self)                               int()函数
+object.__float__(self)                             float()函数
+object.__round__(self[,n])                         round()函数
+object.__trunc__(self[,n])                         math.trunc()函数
+object.__ceil__(self[,n])                          math.ceil()函数
+object.__floor__(self[,n])                         math.floor()函数
+
+反向运算符
+object.__radd__(self,other)                        +
+object.__rsub__(self, other)                       -
+object.__rmul__(self, other)                       *
+object.__rtruediv__(self, other)                   /
+object.__rfloordiv__(self, other)                  //
+object.__rmod__(self, other)                       %
+object.__rdivmod__(self,other)                     divmod()函数
+object.__rpow__(self,other[,modulo])               pow()函数  **
+
+比较运算符
+object.__lt__(self,other)                          <
+object.__le__(self,other)                          <=
+object.__eq__(self,other)                          ==
+object.__ne__(self,other)                          !=
+object.__gt__(self,other)                          >
+object.__ge__(self,other)                          >=
+
+特殊方法
+object.__lshift__(self,other)                      <<
+object.__rshift__(self,other)                      >>
+object.__and__(self,other)                         &
+object.__xor__(self,other)                         ^
+object.__or__(self,other)                          |
+
+object.__rlshift__(self,other)                     <<
+object.__rrshift__(self,other)                     >>
+object.__rand__(self,other)                        &
+object.__rxor__(self,other)                        ^
+object.__ror__(self,other)                         |
+
+object.__invert__(self)                            <<  位运算值的取反操作
+
+原地运算符
+object.__iadd__(self, other)                       +=
+object.__isub__(self, other)                       -=
+object.__imul__(self, other)                       *=
+object.__itruediv__(self, other)                   /=
+object.__ifloordiv__(self, other)                  //=
+object.__imod__(self, other)                       %=
+object.__ipow__(self,other[,modulo])               **=
+object.__ilshift__(self,other)                     <<=
+object.__irshift__(self,other)                     >>=
+object.__iand__(self,other)                        &=
+object.__ixor__(self,other)                        ^=
+object.__ior__(self,other)                         |=
+
+'''
+
